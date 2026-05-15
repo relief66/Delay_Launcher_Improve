d880711f619aa80df5dc110f2ddf7ce14eb9cb70
relief66 <relief66@users.noreply.github.com>
2026-05-15 14:14:13 +0200

    Create AppInfo.java

---
+++ app/src/main/java/com/example/delaylauncherimprove/AppInfo.java
@@ -0,0 +1,33 @@
+package com.example.Delay_Launcher_Improve;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    private final String label;
+    private final String packageName;
+    private final Drawable icon;
+
+    public AppInfo(String label, String packageName, Drawable icon) {
+        this.label = label;
+        this.packageName = packageName;
+        this.icon = icon;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public Drawable getIcon() {
+        return icon;
+    }
+
+    // Fondamentale: lo Spinner usa questo metodo per mostrare la stringa nel menu a tendina
+    @Override
+    public String toString() {
+        return label;
+    }
+}
